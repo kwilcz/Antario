afb1649a2518d457a00099339fa3e21c2cafbdc7
terryadavis <33110055+terryadavis@users.noreply.github.com>
2017-12-02 21:00:24 -0800

    Update dllmain.cpp

--- Antario/dllmain.cpp
+++ Antario/dllmain.cpp
@@ -1,17 +1,34 @@
 ﻿#include "Hooks.h"
 
-BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
-{
-    if (dwReason == DLL_PROCESS_ATTACH) 
-    {
-        DisableThreadLibraryCalls(hModule);
-        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Hooks::Init, NULL, NULL, NULL);
-    }
-    else if (dwReason == DLL_PROCESS_DETACH) 
-    {
-        if (!lpReserved)
-            Hooks::Restore();
-    }
-    return TRUE;
+using ulong_t = unsigned long;
+
+void __stdcall on_attach() { // initialize stuff in here
+    Hooks::Init();
 }
 
+void __stdcall on_detach() { // restore stuff in here
+    Hooks::Restore();
+}
+
+bool __stdcall DllMain(HINSTANCE dll_instance, ulong_t reason_for_call, void *reserved) {
+    switch (reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(dll_instance);
+        CreateThread(0, 0, LPTHREAD_START_ROUTINE(on_attach), 0, 0, 0);
+
+        return true;
+        break;
+
+    case DLL_PROCESS_DETACH:
+        if ( reserved == nullptr ) {
+            on_detach();
+        }
+
+        return true;
+        break;
+
+    default:
+        return false;
+        break;
+    }
+}
