3fc8cb2330c4400340d0e305c3f5bca9bcd2df3d
certmemer <alicedconnors@gmail.com>
2018-01-03 21:56:45 +0100

    changes to color """class"""

--- Antario/SDK/Color.h
+++ Antario/SDK/Color.h
@@ -2,56 +2,43 @@
 
 struct Color
 {
-    int r, g, b, a;
+	int red, green, blue, alpha;
 
-    Color()
-    {
-        this->r = 0;
-        this->g = 0;
-        this->b = 0;
-        this->a = 255;
-    }
+	Color()
+	{
+		this->red = 0;
+		this->green = 0;
+		this->blue = 0;
+		this->alpha = 255;
+	}
 
-    Color(int r, int g, int b, int a = 255)
-    {
-        this->r = r;
-        this->g = g;
-        this->b = b;
-        this->a = a;
-    }
+	Color(int r, int g, int b, int a = 255)
+		: red{ r }, green{ g }, blue{ b }, alpha{ a }
+	{ }
 
-    Color& operator /= (float div)
-    {
-        Color& color = *this;
-        float flDiv = 1.f / div;
-        color.r = static_cast<int>(color.r * flDiv);
-        color.g = static_cast<int>(color.g * flDiv);
-        color.b = static_cast<int>(color.b * flDiv);
-        return color;
-    }
+	Color& operator /= (float div)
+	{
+		Color& color = *this;
+		float flDiv = 1.f / div;
+		color.red = static_cast<int>(color.red * flDiv);
+		color.green = static_cast<int>(color.green * flDiv);
+		color.blue = static_cast<int>(color.blue * flDiv);
+		return color;
+	}
 
-    Color& operator *= (float coeff)
-    {
-        Color& color = *this;
-        color.r = static_cast<int>(color.r * coeff);
-        color.g = static_cast<int>(color.g * coeff);
-        color.b = static_cast<int>(color.b * coeff);
-        return color;
-    }
+	Color& operator *= (float coeff)
+	{
+		Color& color = *this;
+		color.red = static_cast<int>(color.red * coeff);
+		color.green = static_cast<int>(color.green * coeff);
+		color.blue = static_cast<int>(color.blue * coeff);
+		return color;
+	}
 
-	static Color Black() {
-		return Color(0, 0, 0);
-	}
-	static Color White() {
-		return Color(255, 255, 255);
-	}
-	static Color Red() {
-		return Color(255, 0, 0);
-	}
-	static Color Green() {
-		return Color(0, 255, 0);
-	}
-	static Color Blue() {
-		return Color(0, 0, 255);
-	}
+	static Color Black() { return Color(0, 0, 0); }
+	static Color Grey() { return Color(127, 127, 127); }
+	static Color White() { return Color(255, 255, 255); }
+	static Color Red() { return Color(255, 0, 0); }
+	static Color Green() { return Color(0, 255, 0); }
+	static Color Blue() { return Color(0, 0, 255); }
 };
