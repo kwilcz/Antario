054832cb3c76b300dbb00bee66a668206e71750d
certmemer <alicedconnors@gmail.com>
2018-01-03 20:25:31 +0100

    le optimised Eps MEn

--- Antario/Features/ESP.cpp
+++ Antario/Features/ESP.cpp
@@ -7,41 +7,38 @@ ESP g_ESP;
 
 void ESP::RenderName(C_BaseEntity* pEnt, int iterator)
 {
-    Color textColor;
-    if (pEnt->GetTeam() == g::pLocalEntity->GetTeam())
-        textColor = Color(195, 240, 100, 255);
-    else
-        textColor = Color(250, 165, 110, 255);
+	PlayerInfo_t pInfo;
+	g_pEngine->GetPlayerInfo(iterator, &pInfo);
 
 
-    PlayerInfo_t pInfo;
-    g_pEngine->GetPlayerInfo(iterator, &pInfo);
-    std::string strPlayerName = std::string(pInfo.szName);
+	Vector vecPosition = pEnt->GetEyePosition(); vecPosition.z += 30;
+	Vector vecScreenPos;
+	if (!Utils::WorldToScreen(vecPosition, vecScreenPos))
+		return;
 
-
-    Vector vecPosition = pEnt->GetEyePosition(); vecPosition.z += 30;
-    Vector vecScreenPos;
-    if (!Utils::WorldToScreen(vecPosition, vecScreenPos))
-        return;
-
-    g_Render.String(vecScreenPos.x, vecScreenPos.y, CD3DFONT_CENTERED_X | CD3DFONT_DROPSHADOW, textColor, g_Fonts.pFontTahoma10.get(), strPlayerName.c_str());
+	g_Render.String(vecScreenPos.x, vecScreenPos.y,
+		CD3DFONT_CENTERED_X | CD3DFONT_DROPSHADOW,
+		(localTeam == pEnt->GetTeam()) ? teamColor : enemyColor,
+		g_Fonts.pFontTahoma10.get(), pInfo.szName);
 }
 
 
 void ESP::Render()
 {
-    for (int it = 0; it < g_pEngine->GetMaxClients(); ++it)
-    {
-        C_BaseEntity* pPlayerEntity = g_pEntityList->GetClientEntity(it);
+	localTeam = g::pLocalEntity->GetTeam();
 
-        // Sanity checks
-        if (!pPlayerEntity
-            || !pPlayerEntity->IsAlive()
-            || pPlayerEntity->IsDormant()
-            || pPlayerEntity == g::pLocalEntity)
-            continue;
+	for (int it = 0; it < g_pEngine->GetMaxClients(); ++it)
+	{
+		C_BaseEntity* pPlayerEntity = g_pEntityList->GetClientEntity(it);
 
-        if (g_Settings.bShowNames)
-            this->RenderName(pPlayerEntity, it);
-    }
+		// Sanity checks
+		if (!pPlayerEntity
+			|| !pPlayerEntity->IsAlive()
+			|| pPlayerEntity->IsDormant()
+			|| pPlayerEntity == g::pLocalEntity)
+			continue;
+
+		if (g_Settings.bShowNames)
+			this->RenderName(pPlayerEntity, it);
+	}
 }
--- Antario/Features/ESP.h
+++ Antario/Features/ESP.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <deque>
 #include "..\Utils\DrawManager.h"
 #include "..\Utils\GlobalVars.h"
 #include "..\Settings.h"
@@ -8,8 +7,12 @@
 class ESP
 {
 public:
-    void Render();
+	void Render();
 private:
-    void RenderName(C_BaseEntity* pEnt, int iterator);
+	void RenderName(C_BaseEntity* pEnt, int iterator);
+
+	int localTeam;
+	Color teamColor{ 195, 240, 100, 255 };
+	Color enemyColor{ 250, 165, 110, 255 };
 };
 extern ESP g_ESP;
