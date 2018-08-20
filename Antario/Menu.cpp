a1dc324f3ada75a55cfaf621c0827919f7c7ce0e
Wando <kamil531@gmail.com>
2018-08-20 22:06:44 +0200

    Fix for missing parent for sections

--- Antario/Menu.cpp
+++ Antario/Menu.cpp
@@ -7,33 +7,41 @@ void Detach() { g_Settings.bCheatActive = false; }
 
 void MenuMain::Initialize()
 {
-    static int testint;
-    static int testint2;
+    static int   testint;
+    static int   testint2;
+    static int   testint3 = 2;
     static float float123 = 10.f;
-    static int testint3 = 2;
     /* Create our main window (Could have multiple if you'd create vec. for it) */
     auto mainWindow = std::make_shared<BaseWindow>(SPoint(450, 450), SPoint(360, 256), g_Fonts.pFontTahoma8.get(), g_Fonts.pFontTahoma10.get(), "Antario - Main");
     {
+        /* Create sections for it */
         auto sectMain = std::make_shared<BaseSection>(SPoint(310, 100), 2, "Test Section 1");
         {
+            /* Add controls within section */
             sectMain->AddCheckBox("Bunnyhop Enabled", &g_Settings.bBhopEnabled);
             sectMain->AddCheckBox("Show Player Names", &g_Settings.bShowNames);
             sectMain->AddButton("Shutdown", Detach);
             sectMain->AddSlider("TestSlider", &float123, 0, 20);
             sectMain->AddSlider("intslider", &testint3, 0, 10);
             sectMain->AddCombo("TestCombo", std::vector<std::string>{ "Value1", "Value2", "Value3" }, &testint);
+
+            /* At the end / beginng set parent of the section */
+            sectMain->SetParent(mainWindow.get());
         }
+        /* And remember about adding the section as a child of the window */
         mainWindow->AddChild(sectMain);
+
         auto sectMain2 = std::make_shared<BaseSection>(SPoint(310, 100), 2, "Test Section 2");
         {
-            /* To be removed */
             sectMain2->AddCombo("TestCombo2", std::vector<std::string>{ "ttest", "ttest2", "ttest3" }, &testint2);
             sectMain2->AddCheckBox("CheckboxSect2_1", &g_Settings.bShowNames);
             sectMain2->AddCheckBox("CheckboxSect2_2", &g_Settings.bShowNames);
+            sectMain->SetParent(mainWindow.get());
         }
         mainWindow->AddChild(sectMain2);
     }
     this->AddChild(mainWindow);
 
-    mouseCursor = std::make_unique<MouseCursor>();    /* Create our mouse cursor (one instance only) */
+    /* Create our mouse cursor (one instance only) */
+    mouseCursor = std::make_unique<MouseCursor>();    
 }
