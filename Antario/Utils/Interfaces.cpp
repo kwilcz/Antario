8ba17e13ba4eb83beddcb20504229676e2239acd
Pocakking <42542447+Pocakking@users.noreply.github.com>
2018-10-25 15:22:49 +0200

    Versionless Interfaces

--- Antario/Utils/Interfaces.cpp
+++ Antario/Utils/Interfaces.cpp
@@ -11,45 +11,81 @@
 
 // Initializing global interfaces
 
-IBaseClientDLL*     g_pClientDll    = nullptr;
-IClientMode*        g_pClientMode   = nullptr;
-IClientEntityList*  g_pEntityList   = nullptr;
-IVEngineClient*     g_pEngine       = nullptr;
-CPrediction*        g_pPrediction   = nullptr;
-IGameMovement*      g_pMovement     = nullptr;
-IMoveHelper*        g_pMoveHelper   = nullptr;
-CGlobalVarsBase*    g_pGlobalVars   = nullptr;
+IBaseClientDLL*     g_pClientDll = nullptr;
+IClientMode*        g_pClientMode = nullptr;
+IClientEntityList*  g_pEntityList = nullptr;
+IVEngineClient*     g_pEngine = nullptr;
+CPrediction*        g_pPrediction = nullptr;
+IGameMovement*      g_pMovement = nullptr;
+IMoveHelper*        g_pMoveHelper = nullptr;
+CGlobalVarsBase*    g_pGlobalVars = nullptr;
 IGameEventManager2* g_pEventManager = nullptr;
-ISurface*           g_pSurface      = nullptr;
+ISurface*           g_pSurface = nullptr;
 
+#pragma pack(push, 1)
+struct GameInterface
+{
+	void*(*func)();
+	char* name;
+	GameInterface* next;
+};
+#pragma pack(pop)
+
+static GameInterface* GetInterfaceListFromFunction(CreateInterfaceFn func)
+{
+	return *(GameInterface**)(*(std::uintptr_t*)(((std::uintptr_t)func) - 0x6A));
+}
+
+static bool StartsWith(char const* str, char const* target)
+{
+	for (; *str && *target; ++str, ++target)
+	{
+		if (*str != *target)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
 
 namespace interfaces
 {
-    template<typename T>
-    T* CaptureInterface(const char* szModuleName, const char* szInterfaceVersion)
-    {
-        HMODULE moduleHandle = GetModuleHandleA(szModuleName);
-        if (moduleHandle)   /* In case of not finding module handle, throw an error. */
-        {
-            CreateInterfaceFn pfnFactory = reinterpret_cast<CreateInterfaceFn>(GetProcAddress(moduleHandle, "CreateInterface"));
-            return reinterpret_cast<T*>(pfnFactory(szInterfaceVersion, nullptr));
-        }
-        Utils::Log(std::string("Error getting interface ") + std::string(szInterfaceVersion));
-        return nullptr;
-    }
+	template<typename T>
+	T* CaptureInterface(const char* szModuleName, const char* szInterfaceVersion)
+	{
+		HMODULE moduleHandle = GetModuleHandleA(szModuleName);
+		if (moduleHandle)   /* In case of not finding module handle, throw an error. */
+		{
+			CreateInterfaceFn pfnFactory = reinterpret_cast<CreateInterfaceFn>(GetProcAddress(moduleHandle, "CreateInterface"));
+			if (pfnFactory)
+			{
+				auto i = GetInterfaceListFromFunction(pfnFactory);
+				do
+				{
+					if (StartsWith(i->name, szInterfaceVersion))
+					{
+						return static_cast<T*>(i->func());
+					}
+				} while (i = i->next);
+			}
+		}
+		Utils::Log(std::string("Error getting interface ") + szInterfaceVersion);
+		return nullptr;
+	}
 
 
-    void Init()
-    {
-        g_pClientDll    = CaptureInterface<IBaseClientDLL>("client_panorama.dll", "VClient018");					// Get IBaseClientDLL
-        g_pClientMode   = **reinterpret_cast<IClientMode***>    ((*reinterpret_cast<uintptr_t**>(g_pClientDll))[10] + 0x5u);  // Get IClientMode
-        g_pGlobalVars   = **reinterpret_cast<CGlobalVarsBase***>((*reinterpret_cast<uintptr_t**>(g_pClientDll))[0]  + 0x1Bu); // Get CGlobalVarsBase
-        g_pEntityList   = CaptureInterface<IClientEntityList>("client_panorama.dll", "VClientEntityList003");    // Get IClientEntityList
-        g_pEngine       = CaptureInterface<IVEngineClient>("engine.dll", "VEngineClient014");						// Get IVEngineClient
-        g_pPrediction   = CaptureInterface<CPrediction>("client_panorama.dll", "VClientPrediction001");          // Get CPrediction
-        g_pMovement     = CaptureInterface<IGameMovement>("client_panorama.dll", "GameMovement001");             // Get IGameMovement
-        g_pMoveHelper   = **reinterpret_cast<IMoveHelper***>((Utils::FindSignature("client_panorama.dll", "8B 0D ? ? ? ? 8B 46 08 68") + 0x2));  // Get IMoveHelper
-        g_pEventManager = CaptureInterface<IGameEventManager2>("engine.dll", "GAMEEVENTSMANAGER002");				// Get IGameEventManager2
-        g_pSurface      = CaptureInterface<ISurface>("vguimatsurface.dll", "VGUI_Surface031");						// Get ISurface
-    }
+	void Init()
+	{
+		g_pClientDll = CaptureInterface<IBaseClientDLL>("client_panorama.dll", "VClient0");					// Get IBaseClientDLL
+		g_pClientMode = **reinterpret_cast<IClientMode***>    ((*reinterpret_cast<std::uintptr_t**>(g_pClientDll))[10] + 0x5u);  // Get IClientMode
+		g_pGlobalVars = **reinterpret_cast<CGlobalVarsBase***>((*reinterpret_cast<std::uintptr_t**>(g_pClientDll))[0] + 0x1Bu); // Get CGlobalVarsBase
+		g_pEntityList = CaptureInterface<IClientEntityList>("client_panorama.dll", "VClientEntityList");    // Get IClientEntityList
+		g_pEngine = CaptureInterface<IVEngineClient>("engine.dll", "VEngineClient0");					  // Get IVEngineClient
+		g_pPrediction = CaptureInterface<CPrediction>("client_panorama.dll", "VClientPrediction");          // Get CPrediction
+		g_pMovement = CaptureInterface<IGameMovement>("client_panorama.dll", "GameMovement");             // Get IGameMovement
+		g_pMoveHelper = **reinterpret_cast<IMoveHelper***>((Utils::FindSignature("client_panorama.dll", "8B 0D ? ? ? ? 8B 46 08 68") + 0x2));  // Get IMoveHelper
+		g_pEventManager = CaptureInterface<IGameEventManager2>("engine.dll", "GAMEEVENTSMANAGER002");		  // Get IGameEventManager2
+		g_pSurface = CaptureInterface<ISurface>("vguimatsurface.dll", "VGUI_Surface");					  // Get ISurface
+	}
 }
